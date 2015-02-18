34c0f84ef3315e7ad9961baaa416e1380db8f85f
soyabshaikh <soyab.s@applicationnexus.com>
2015-02-18 11:45:02 +0530

    Update WizUtilsPlugin.java

--- android/src/jp/wizcorp/phonegap/plugin/wizUtilsPlugin/WizUtilsPlugin.java
+++ android/src/jp/wizcorp/phonegap/plugin/wizUtilsPlugin/WizUtilsPlugin.java
@@ -151,7 +151,28 @@ public class WizUtilsPlugin extends CordovaPlugin {
                 }
             });
             return true;
-        }
+        }else if(action.equals("getDeviceName"))
+        {
+           
+       String manufacturer = Build.MANUFACTURER;
+       String model = Build.MODEL;
+       if (model.startsWith(manufacturer)) {
+          return capitalize(model);
+       } else {
+          return capitalize(manufacturer) + " " + model;
+       }
+    }
         return false;
     }
+    private String capitalize(String s) {
+    if (s == null || s.length() == 0) {
+        return "";
+    }
+    char first = s.charAt(0);
+    if (Character.isUpperCase(first)) {
+        return s;
+    } else {
+        return Character.toUpperCase(first) + s.substring(1);
+    }
+}
 }
